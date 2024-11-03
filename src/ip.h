ea00f48ecf7e913e328955cb6f9db3220a40f67b
Me1k0r <runner-me1k0r@proton.me>
2024-11-03 14:58:22 +0100

    edit: ip - ipv4 base struct + mask with cidr bit fields

---
+++ src/ip.h
@@ -0,0 +1,42 @@
+// ####################################################################################
+// IPV4
+
+/**
+ * @brief Base IPv4
+ * 
+ */
+struct ipv4base
+{
+  // IP address bytes
+  unsigned char bytes[4];
+};
+
+typedef struct ipv4mask
+{
+  struct ipv4base address;
+  unsigned char cidr : 6;  
+} ipv4mask;
+
+typedef struct ipv4
+{
+  struct ipv4base address;
+  ipv4mask mask;  
+} ipv4;
+
+/**
+ * @brief Creates a new IP address from provided bytes
+ * 
+ * @param b1 {unsigned char} : first byte
+ * @param b2 {unsigned char} : second byte
+ * @param b3 {unsigned char} : third byte
+ * @param b4 {unsigned char} : fourth byte
+ * @return ip* : new IP address
+ */
+ip *createIPAddressFromBytes(unsigned char b1, unsigned char b2, unsigned char b3, unsigned char b4);
+
+/**
+ * @brief Prints ip address
+ * 
+ * @param addr {ip} : ip to print
+ */
+void printIP(ip *addr);
